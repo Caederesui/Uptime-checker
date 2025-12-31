64c2fc31466d5fe53d947e2d3f6cf45408ee4697
Fristaylo <andru_2004@mail.ru>
2026-01-01 02:18:11 +0400

    Fix

--- client/src/components/DomainStatus/DomainStatus.tsx
+++ client/src/components/DomainStatus/DomainStatus.tsx
@@ -123,10 +123,9 @@ const DomainStatus: React.FC<DomainStatusProps> = ({ domain, logs }) => {
                                                     :{" "}
                                                     {r.status_code !== null
                                                         ? `Статус: ${r.status_code}`
-                                                        : "Неизвестный статус"}
-                                                    ;{" "}
+                                                        : "неизвестный статус"}
                                                     {r.total_time !== null
-                                                        ? `Время: ${r.total_time}ms`
+                                                        ? `; Время: ${r.total_time}ms`
                                                         : ""}
                                                 </div>
                                             ))}
